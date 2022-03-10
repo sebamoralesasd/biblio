8aee5119267b14680989ab7333c1ae90e63b00cc
Sebastián Morales <sebasuni96@gmail.com>
2022-03-10 20:04:40 -0300

    Commit inicial

---
+++ db/migrate/20220310225410_create_books.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class CreateBooks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+
+      t.timestamps
+    end
+  end
+end
